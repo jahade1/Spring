bdde6d6edb25ab162d1ab2aa74a3777cdad83caa
jahad <jahad@DESKTOP-OO4A538.home>
2019-03-17 12:47:50 -0400

    AutoWired setter method

--- spring-demo-annotation/src/com/jahadenglish/springAnnotation/TennisCoach.java
+++ spring-demo-annotation/src/com/jahadenglish/springAnnotation/TennisCoach.java
@@ -8,10 +8,18 @@ public class TennisCoach implements Coach {
 
 	private FortuneService fortuneService;
 	
+	
+	public TennisCoach() {
+		System.out.println(">>Tennis Coach: inside default constructor ");
+	}
+	
+	//create setter for setter injection
 	@Autowired
-	public TennisCoach(FortuneService fortuneService) {
+	public void setFortuneService(FortuneService fortuneService) {
+		System.out.println(">>Tennis Coach: inside setter method");
 		this.fortuneService = fortuneService;
 	}
+	
 	@Override
 	public String getDailyWorkout() {
 	
